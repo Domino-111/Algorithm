dad02b113e7e3d2bcc290a8fa35b7b59f9e56f35
Domino-111 <dom.walsh@outlook.com>
2025-03-19 16:28:57 +1100

    Week7

--- Assets/Scripts/LinkedList/MyLinkedList.cs
+++ Assets/Scripts/LinkedList/MyLinkedList.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
+using Unity.VisualScripting;
 
 namespace OurLinkedList
 {
@@ -8,6 +10,7 @@ namespace OurLinkedList
         public int age = 0;
 
         public Node next;
+        public Node prev;
 
         public Node(string name, int age)
         {
@@ -27,6 +30,7 @@ namespace OurLinkedList
             Node BrainDead = new Node("Gen A", 13);
 
             LinkedList linkedList = new LinkedList(Andrew);
+            linkedList.InsertPrev(Anthony);
             linkedList.InsertNext(Anthony);
             linkedList.InsertNext(BrainDead);
 
@@ -48,12 +52,15 @@ namespace OurLinkedList
         private Node current;
 
         private Node header;
+        private Node tail;
 
         public LinkedList(Node node)
         {
             header = node;
+            tail = node;
             current = node;
             header.next = null;
+            header.prev = null;
         }
 
         public void InsertNext(Node newNode)
@@ -61,34 +68,120 @@ namespace OurLinkedList
             if (current.next == null)
             {
                 current.next = newNode;
+                newNode.prev = current;
                 newNode.next = null;
+                tail = newNode;
             }
             else
             {
                 newNode.next = current.next;
                 current.next = newNode;
+
+                newNode.next.prev = newNode;
+                newNode.prev = current;
             }
-            current = current.next;
+            current = newNode;
         }
 
-        public void DeleteNext()
+        public void InsertPrev(Node newNode)
+        {
+            if (current.prev == null)
+            {
+                header = newNode;
+                current.prev = newNode;
+                newNode.next = current;
+                newNode.prev = null;
+            }
+            else
+            {
+                newNode.prev = current.prev;
+                current.prev = newNode;
+
+                newNode.prev.next = newNode;
+                newNode.next = current;
+            }
+            current = newNode;
+        }
+
+        public bool DeleteNext()
         {
             if (current.next == null)
             {
-                return;
+                return false;
             }
 
             //Node delNode = current.next;
             current.next = current.next.next;
+            
+            if (current.next == null)
+            {
+                tail = current;
+            }
+            else
+            {
+                current.next.prev = current;
+            }
 
             //delNode = null;
+            return true;
+        }
+
+        public bool DeletePrev()
+        {
+            if (current.prev == null)
+            {
+                return false;
+            }
+
+            //Node delNode = current.next;
+            current.prev = current.prev.prev;
+
+            if (current.prev == null)
+            {
+                header = current;
+            }
+            else
+            {
+                current.prev.next = current;
+            }
+
+            //delNode = null;
+            return true;
+        }
+
+        public void DeleteCurrent()
+        {
+            if (Previous())
+            {
+                DeleteNext();
+            }
+            else if (Next())
+            {
+                DeletePrev();
+            }
+            else
+            {
+                header = null;
+                tail = null;
+                current = null;
+            }
         }
 
         public bool Next()
         {
-            if (current.next == null)
+            if (current.next != null)
             {
                 current = current.next;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Previous()
+        {
+            if (current.prev != null)
+            {
+                current = current.prev;
                 return true;
             }
             return false;
@@ -122,6 +215,23 @@ namespace OurLinkedList
             {
                 Print(currentPrint);
                 currentPrint = currentPrint.next;
+            }
+            while (currentPrint != null);
+        }
+
+        public void PrintBackwardsAll()
+        {
+            if (tail == null)
+            {
+                return;
+            }
+
+            Node currentPrint = tail;
+
+            do
+            {
+                Print(currentPrint);
+                currentPrint = currentPrint.prev;
             }
             while (currentPrint != null);
         }
